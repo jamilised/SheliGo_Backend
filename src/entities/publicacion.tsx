c4f403982210fa146abb2495d9c0b42090ca7a56
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-05-15 10:30:26 -0300

    entities/publicacion.tsx

---
+++ src/entities/publicacion.tsx
@@ -0,0 +1,19 @@
+export default class Publicacion {
+
+    constructor(
+        public id: number,
+        public usuario_id: number,
+        public institucion_id: number | null,
+        public categoria_id: number | null,
+        public archivos_id: number | null,
+        public nombre: string,
+        public descripcion: string | null,
+        public fecha_evento: string,
+        public latitud: number | null,
+        public longitud: number | null,
+        public created_at: string,
+        public updated_at: string,
+        public tipo: string,
+        public estado: string
+    ) {}
+}
