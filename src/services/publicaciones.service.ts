78950af41f2c8a07ece3349b1f9f5834f3fe8a46
Maica <maica.trutner@gmail.com>
2026-05-23 15:29:13 -0300

    intituciones-srvice y publicaciones-service getRecent

--- src/services/publicaciones.service.ts
+++ src/services/publicaciones.service.ts
@@ -1,5 +1,6 @@
 import PublicacionesRepository from '../repositories/publicaciones.repository.js'
 import NotFoundError from '../errors/not-found-error.js'
+import AppError from '../errors/app-error.js';
 
 class PublicacionesService {
 
@@ -19,6 +20,16 @@ class PublicacionesService {
         return publicacion
 
     }
+
+    getRecentPublicaciones = async () => {
+        const publicaciones = await this.repository.getRecent();
+
+        if (publicaciones === null) {
+            throw new AppError('Error al recuperar las publicaciones recientes', 500);
+        }
+
+        return publicaciones;
+    };
 
 }
 
