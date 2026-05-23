8b843612b259dd139093af9bf90e5f6a2d8eebd7
Maica <maica.trutner@gmail.com>
2026-05-23 15:29:13 -0300

    intituciones-srvice y publicaciones-service getRecent

---
+++ src/services/instituciones-service.ts
@@ -0,0 +1,18 @@
+import InstitucionesRepository from '../repositories/instituciones-repository.js';
+import AppError from '../errors/app-error.js';
+
+class InstitucionesService {
+    private institucionesRepo = new InstitucionesRepository();
+
+    getRecentInstituciones = async () => {
+        const instituciones = await this.institucionesRepo.getRecent();
+
+        if (instituciones === null) {
+            throw new AppError('Error al recuperar las instituciones recientes', 500);
+        }
+
+        return instituciones;
+    };
+}
+
+export default InstitucionesService;
