4a4b387ca8ae40c37006765abec5b3cfa3200a67
Maica <maica.trutner@gmail.com>
2026-05-23 16:46:39 -0300

    publicaciones.router get home

--- src/routers/publicaciones.router.ts
+++ src/routers/publicaciones.router.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express'
 import publicacionesController
 from '../controllers/publicaciones.controller.js'
+import { authMiddleware } from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
 router.get(
     '/:id',
     publicacionesController.getPublicacionDetalle
+)
+
+router.get(
+    '/home', 
+    authMiddleware, 
+    publicacionesController.getHomeData
 )
 
 export default router
