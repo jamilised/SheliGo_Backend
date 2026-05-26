ad387285b443789459c4fd32a2fa150050df6fdc
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-05-26 11:22:42 -0300

    comentarios middleware corregidos

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -7,18 +7,18 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
         const authHeader = req.headers.authorization
 
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            // Si no viene el token, cortamos la petición acá con un 401 (No autorizado)
+            // Si no viene el token, cortamos la petición acá con un 401
             throw new AppError('Acceso denegado. No se proporcionó un token válido.', 401)
         }
 
         const token = authHeader.split(' ')[1]
 
         // --- NOTA PARA NOSOTROS ---
-        // Acá tu grupo deberá usar JWT (jsonwebtoken) o Supabase Auth para verificar el token real:
+        // Deberíamos usar JWT (jsonwebtoken) o Supabase Auth para verificar el token real:
         // const decoded = jwt.verify(token, process.env.JWT_SECRET)
         // const userId = decoded.id
         
-        // De forma provisoria para probar tu pantalla, simulamos que el token es el mismo ID de usuario:
+        // Por ahora simulamos que el token es el mismo ID de usuario:
         res.locals.userIdLogged = token
 
         return next()
