c8fab3612622e4a0b03b9f8709631deaae392a75
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-05-26 11:44:19 -0300

    home-router

---
+++ src/routers/home-router.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import homeController
+from '../controllers/home-controller.js'
+import { authMiddleware } from '../middlewares/auth-middleware.js'
+
+const router = Router()
+
+router.get(
+    '/home', 
+    authMiddleware, 
+    homeController.getHomeData
+)
