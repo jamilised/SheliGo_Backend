fed449f091781e1ff88b8bc3006a80a916a5b2c6
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-05-26 11:44:19 -0300

    home-router

--- src/routers/publicaciones-router.ts
+++ src/routers/publicaciones-router.ts
@@ -1,15 +1,8 @@
 import { Router } from 'express'
 import publicacionesController
 from '../controllers/publicaciones-controller.js'
-import { authMiddleware } from '../middlewares/auth-middleware.js'
 
 const router = Router()
-
-router.get(
-    '/home', 
-    authMiddleware, 
-    publicacionesController.getHomeData
-)
 
 router.get(
     '/:id',
