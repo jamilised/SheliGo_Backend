7206f7c73e04bafb34ae0df803a21daf963ea09e
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-05-26 12:13:07 -0300

    header agregado a app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
 import express from 'express'
-import publicacionesRouter
-from './routers/publicaciones-router.js'
+import publicacionesRouter from './routers/publicaciones-router.js'
+import headerRouter from './routers/header-router.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/publicaciones', publicacionesRouter)
+app.use('/header', headerRouter)
 
 export default app
-
-
-
-
