0617a56d375595538be216469ede9bfd42af0e0f
MORENA YAEL ZALCMAN <49089410@ort>
2026-05-29 09:03:23 -0300

    app.ts para conectar con front

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,49 @@
-import express from 'express'
-import publicacionesRouter from './routers/publicaciones-router.js'
-import headerRouter from './routers/header-router.js'
-import homeRouter from './routers/home-router.js'
+import express from "express";
+import cors from "cors";
 
-const app = express()
+import publicacionesRouter
+from "./routers/publicaciones-router.js";
 
-app.use(express.json())
+import headerRouter
+from "./routers/header-router.js";
 
-app.use('/publicaciones', publicacionesRouter)
-app.use('/header', headerRouter)
-app.use('/home', homeRouter)
+import homeRouter
+from "./routers/home-router.js";
 
-export default app
+const app = express();
+
+/*
+Permite que el frontend
+(Vite React)
+pueda consumir el backend.
+*/
+
+app.use(
+
+  cors({
+
+    origin:
+      "http://localhost:5173",
+
+  })
+
+);
+
+app.use(express.json());
+
+app.use(
+  "/publicaciones",
+  publicacionesRouter
+);
+
+app.use(
+  "/header",
+  headerRouter
+);
+
+app.use(
+  "/home",
+  homeRouter
+);
+
+export default app;
