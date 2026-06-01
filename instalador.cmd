3b7a50cfcef59a5942b789ad2168a2bff506794c
Android Studio <devandroid@ort>
2026-06-01 08:59:22 -0300

    instalador.cmd

---
+++ instalador.cmd
@@ -0,0 +1,11 @@
+npm install
+npm install express cors dotenv pg zod
+npm install @supabase/supabase-js
+npm install -D typescript ts-node-dev
+npm install -D @types/express
+npm install -D @types/node
+npm install -D tsx
+npm install @supabase/supabase-js @supabase/ssr
+npm install postgres
+npm install --save-dev @types/cors
+npx skills add supabase/agent-skills
