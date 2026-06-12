b7203c2eb2cf47de4e12000e806b594906ab13c1
ELIEL DAN JAMILIS <49086719@ort>
2026-06-12 10:09:57 -0300

    Implemento el authMiddleware en todos los enpoints 'privados', validación de token

--- src/routers/publicaciones-router.ts
+++ src/routers/publicaciones-router.ts
@@ -8,27 +8,33 @@ from '../controllers/archivos-controller.js'
 import PreguntasController
 from '../controllers/preguntas-controller.js'
 
+import { authMiddleware } from '../middlewares/authMiddleware.js'
+
 const router = Router()
 
 router.get(
     '/:id',
+    authMiddleware,
     publicacionesController.getPublicacionDetalle
 )
 
 router.get(
     '/:id/archivos',
+    authMiddleware,
     ArchivosController
         .getPublicacionArchivos
 )
 
 router.get(
     '/:id/preguntas',
+    authMiddleware,
     PreguntasController
         .getPreguntas
 )
 
 router.post(
     '/:id/preguntas',
+    authMiddleware,
     PreguntasController
         .createPregunta
 )
