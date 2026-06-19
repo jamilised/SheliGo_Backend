c16720c52fbb761d9e28dff0d96cb5a973520b76
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-06-19 08:28:08 -0300

    cambios de nombres de archivos

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import homeRouter
 from "./routers/home-router.js";
 
 import authRouter 
-from "./routers/auth-router.js";
+from "./routers/register-router.js";
 
 import { errorMiddleware } from './middlewares/error-middleware.js';
 
--- src/routers/register-router.ts
+++ src/routers/register-router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import multer from 'multer'
-import authController from '../controllers/auth-controller.js'
+import authController from '../controllers/register-controller.js'
 
 const router = Router()
 
