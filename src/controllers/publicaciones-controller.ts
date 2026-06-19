aa9f10f2e758b1e7795ae65ad1c6103e2f6a47eb
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-06-19 12:07:16 -0300

    publicaciones controller

--- src/controllers/publicaciones-controller.ts
+++ src/controllers/publicaciones-controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from 'express'
 import PublicacionesService from '../services/publicaciones-service.js'
+import { searchPublicacionSchema } from '../validations/publicacion-schema.js';
 
 const getPublicacionDetalle = async (
     req: Request,
@@ -35,6 +36,41 @@ const getPublicacionDetalle = async (
 
 }
 
+const search = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            console.log('C1: Iniciando búsqueda de publicaciones con Query Params:', req.query);
+
+            // 1. Validamos lo que viene por la URL
+            const validacion = searchPublicacionSchema.safeParse(req.query);
+            
+            if (!validacion.success) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'Filtros de búsqueda inválidos',
+                    errors: validacion.error.format() // Le dice al front exactamente qué mandó mal
+                });
+            }
+
+            // 2. Si pasó la validación, agarramos los datos limpios
+            const filtros = validacion.data;
+
+            // 3. Llamamos al Service
+            // 3. Llamamos al Service pasándole los filtros limpios
+            const publicaciones = await PublicacionesService.searchPublicaciones(filtros as any);
+
+            // 4. Respondemos con éxito
+            return res.status(200).json({
+                success: true,
+                data: publicaciones
+            });
+
+        } catch (error) {
+            console.error('ERROR en publicacionesController.search:', error);
+            next(error);
+        }
+    }
+
 export default {
-    getPublicacionDetalle
+    getPublicacionDetalle,
+    search
 }
