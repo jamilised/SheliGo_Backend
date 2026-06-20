398b876cf54d6770af81a23069092a504f62263a
Maica <maica.trutner@gmail.com>
2026-06-20 11:26:54 -0300

    agregado el authmiddleware en instituciones-router

--- src/routers/instituciones-router.ts
+++ src/routers/instituciones-router.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import institucionesController from '../controllers/instituciones-controller.js';
+import { authMiddleware } from '../middlewares/auth-middleware.js';
 
 const router = Router();
 
 // GET /api/instituciones/recientes
-router.get('/recientes', institucionesController.getRecientes);
+router.get('/recientes', authMiddleware, institucionesController.getRecientes);
 
 // GET /api/instituciones (Trae todas)
-router.get('/', institucionesController.getAll);
+router.get('/', authMiddleware, institucionesController.getAll);
 
 export default router;
