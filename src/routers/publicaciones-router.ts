e3d3fc33c9dde38ea42ab17098b991748122ccc1
Maica <maica.trutner@gmail.com>
2026-06-20 11:24:23 -0300

    ahora todo los routers de publicaciones usan authmiddleware

--- src/routers/publicaciones-router.ts
+++ src/routers/publicaciones-router.ts
@@ -6,11 +6,8 @@ import { searchPublicacionSchema } from '../validations/publicacion-schema.js';
 
 const router = Router();
 
-// 📢 Rutas Públicas (No requieren login)
-router.get('/recientes', publicacionesController.getRecientes);
-router.get('/search', validateQuery(searchPublicacionSchema), publicacionesController.search);
-
-// 🔒 Rutas Privadas (Requieren token válido)
+router.get('/recientes', authMiddleware, publicacionesController.getRecientes);
+router.get('/search', authMiddleware, publicacionesController.search);
 router.get('/:id', authMiddleware, publicacionesController.getDetalle);
 router.get('/:id/archivos', authMiddleware, publicacionesController.getArchivos);
 router.get('/:id/preguntas', authMiddleware, publicacionesController.getPreguntas);
