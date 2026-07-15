bdbb625afe645d63a636932d8ffcdbc93aa4237c
Maica <maica.trutner@gmail.com>
2026-07-15 11:59:17 -0300

    app.ts cambiado para que chat ponga bine los errores

--- src/app.ts
+++ src/app.ts
@@ -16,11 +16,12 @@ import categoriasRouter
 import institucionesRouter 
   from './routers/instituciones-router.js';
 
-import { errorMiddleware } 
-  from './middlewares/error-middleware.js';
-
 import chatRouter 
   from './routers/chat-router.js';
+
+
+import { errorMiddleware } 
+  from './middlewares/error-middleware.js';
 
 const app = express();
 
@@ -69,12 +70,12 @@ app.use(
 );
 
 app.use(
-  errorMiddleware
+  '/chat', 
+  chatRouter
 );
 
 app.use(
-  '/chat', 
-  chatRouter
+  errorMiddleware
 );
 
 export default app;
